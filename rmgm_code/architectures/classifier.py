fbe30fda9f609b8a14762eed57a72a36bb8e9195
MrIceHavoc <afonso.fernandes100@gmail.com>
2023-05-09 07:01:06 +0100

    Implemented classifier

--- rmgm_code/architectures/classifier.py
+++ rmgm_code/architectures/classifier.py
@@ -2,8 +2,9 @@ from torch import nn
 from torch.nn import functional as F
 
 class Classifier(nn.Module):
-    def __init__(self, latent_dim) -> None:
+    def __init__(self, latent_dim, model) -> None:
         super().__init__()
+        self.model = model
         self.layers = nn.Sequential(
             nn.Linear(latent_dim, 256),
             F.relu(),
@@ -13,4 +14,10 @@ class Classifier(nn.Module):
         )
 
     def forward(self, x):
-        return self.layers(x)
+        encoding = self.model(x)
+        classification = F.log_softmax(self.layers(encoding[0]))
+        return classification, encoding
+    
+    def loss(self, y_pred, label):
+        one_hot_label = F.one_hot(label, num_classes=len(y_pred))
+        return F.nll_loss(y_pred, one_hot_label)
