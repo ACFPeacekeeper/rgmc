5ee26e3c2b5a6979435d29238613acc5c908654c
MrIceHavoc <afonso.fernandes100@gmail.com>
2023-05-12 04:23:33 +0100

    Simplified noise/attacks

--- rmgm_code/input_transformations/fgsm.py
+++ rmgm_code/input_transformations/fgsm.py
@@ -6,63 +6,29 @@ from tqdm import tqdm
 from input_transformations.adversarial_attack import AdversarialAttack
 
 class FGSM(AdversarialAttack):
-    def __init__(self, device, model, eps=8/255):
+    def __init__(self, device, model, target_modality, eps=8/255):
         super().__init__("FGSM", model)
         self.eps = eps
         self.device = device
+        self.target_modality = target_modality
 
-    def example_generation(self, batch, targets, target_modality):
+    def example_generation(self, x, targets):
         loss = nn.MSELoss().cuda(self.device)
-        if target_modality == 'image':
-            x_adv = [torch.empty(len(batch[0][0]))]*len(batch)
-            x_same = [torch.empty(len(batch[0][1]))]*len(batch)
-        elif target_modality == 'trajectory':
-            x_adv = [torch.empty(len(batch[0][1]))]*len(batch)
-            x_same = [torch.empty(len(batch[0][0]))]*len(batch)
-        else:
-            raise ValueError
-        
-        if len(targets[0]) == 1:
-            x_hat = self.model(batch)
-        else:
-            x_hat, _ = self.model(batch)
 
-        
-        for idx in range(len(batch)):
-            if target_modality == 'image':
-                batch[idx][0].requires_grad = True
-                cost = loss(x_hat[idx][0], targets[idx][0])
-                grad = torch.autograd.grad(cost, batch[idx][0], retain_graph=False, create_graph=False)[0]
-                x_adv_sample = batch[idx][0] + self.eps * grad.sign()
-                x_same[idx] = batch[idx][1]
-            elif target_modality == 'trajectory':
-                batch[idx][1].requires_grad = True
-                cost = loss(x_hat[idx][1], targets[idx][1])
-                grad = torch.autograd.grad(cost, batch[idx][1], retain_graph=False, create_graph=False)[0]
-                x_adv_sample = batch[idx][1] + self.eps * grad.sign()
-                x_same[idx] = batch[idx][0]
-            else:
-                one_hot_label = F.one_hot(targets[idx], num_classes=len(x_hat[idx]))
-                cost = F.nll_loss(x_hat[idx], one_hot_label)
-                if target_modality == 'image':
-                    batch[idx][0].requires_grad = True
-                    grad = torch.autograd.grad(cost, batch[idx][0], retain_graph=False, create_graph=False)[0]
-                    x_adv_sample = batch[idx][0] + self.eps * grad.sign()
-                    x_same[idx] = batch[idx][1]
-                elif target_modality == 'trajectory':
-                    batch[idx][1].requires_grad = True
-                    grad = torch.autograd.grad(cost, batch[idx][1], retain_graph=False, create_graph=False)[0]
-                    x_adv_sample = batch[idx][1] + self.eps * grad.sign()
-                    x_same[idx] = batch[idx][0]
-                
-            x_adv[idx] = torch.clamp(x_adv_sample, 0, 1)
+        for modality in x.keys():
+            x[modality].requires_grad = True
+            
+        x_adv = torch.empty(targets.size())
+        x_hat, _ = self.model(x)
 
-        if target_modality == 'image':
-            x = list(zip(x_adv, x_same))
-        elif target_modality == 'trajectory':
-            x = list(zip(x_same, x_adv))
-        
-        return x
+        print(type(x_hat[self.target_modality]))
+        print(type(targets))
+        cost = loss(x_hat[self.target_modality], targets)
+        grad = torch.autograd.grad(cost, x[self.target_modality], retain_graph=False, create_graph=False)
+
+        x_adv = x[self.target_modality] + self.eps * grad.sign()
+
+        return torch.clamp(x_adv, 0., 1.)
     
     def __repr__(self):
         return self.__class__.__name__ + '(epsilon={0})'.format(self.eps)
--- rmgm_code/input_transformations/gaussian_noise.py
+++ rmgm_code/input_transformations/gaussian_noise.py
@@ -9,35 +9,8 @@ class GaussianNoise(Noise):
 
         self.device = device
 
-    def add_noise(self, x, target_modality):
-        if len(x[0]) == 2:
-            if target_modality == 'image':
-                x_noisy = [torch.empty(x[0].size())]*len(x)
-                x_same = [torch.empty(x[1].size())]*len(x)
-                for idx, x_sample in enumerate(x):
-                    x_noisy[idx] = x_sample[0] + torch.randn(x_sample[0].size()).to(self.device) * self.std + self.mean
-                    x_same[idx] = x_sample[1]
-                x_trans = list(zip(x_noisy, x_same))
-            elif target_modality == 'traj':
-                x_noisy = [torch.empty(len(x[0][1]))]*len(x)
-                x_same = [torch.empty(len(x[0][0]))]*len(x)
-                for x_sample in x:
-                    x_noisy = x_sample[1] + torch.randn(x_sample[1].size()).to(self.device) * self.std + self.mean
-                    x_same = x_sample[0]
-                x_trans = list(zip(x_same, x_noisy))
-            else:
-                raise ValueError
-        else:
-            print(x[0].size())
-            x_noisy = [torch.empty(x[0].size())]*len(x)
-            for idx, x_sample in enumerate(x):
-                
-                x_noisy[idx] = x_sample[0] + torch.randn(x_sample[0].size()).to(self.device) * self.std + self.mean
-            x_trans = list(x_noisy)
-            print(x_trans[0].size())
-
-
-        return x_trans
+    def add_noise(self, x):
+        return x + torch.randn(x.size()).to(self.device) * self.std + self.mean
 
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
