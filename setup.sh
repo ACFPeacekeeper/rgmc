b64bfdaddb234fd4ea53a6d964ed15f946aea1b4
MrIceHavoc <afonso.fernandes100@gmail.com>
2023-05-15 19:53:48 +0100

    Updated env, gitignore and setup files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 CUR_DIR=$(pwd)
 DATA_DIR=${CUR_DIR}/rmgm_code/datasets
-GMC_DIR=${CUR_DIR}/gmc/gmc_code
+GMC_DIR=${CUR_DIR}/rmgm_code/gmc/gmc_code
+MODELS_DIR=${CUR_DIR}/rmgm_code/architectures
+
+if ! [ -d "$GMC_DIR" ]; then
+    echo "Cloning GMC..."
+    git clone "https://github.com/miguelsvasco/gmc.git" ${CUR_DIR}/rmgm_code/gmc
+    echo "Completed! $GMC_DIR directory clone from repository."
+fi
+
+FILE=${MODELS_DIR}/gmc_networks.py
+if ! [ -f "$FILE" ]; then
+    echo "Copying gmc_networks.py to ${MODELS_DIR} directory..."
+    cp ${GMC_DIR}/unsupervised/architectures/models/gmc_networks.py ${FILE}
+    echo "Completed! ${FILE} created."
+fi
+
+FILE=${MODELS_DIR}/gmc.py
+if ! [ -f "$FILE" ]; then
+    echo "Copying gmc.py to ${MODELS_DIR} directory..."
+    cp ${GMC_DIR}/unsupervised/architectures/models/gmc.py ${FILE}
+    sed -i -e "s|^from gmc_code.*$|from architectures.gmc_networks import \*\n|g" ${MODELS_DIR}/gmc.py
+    echo "Completed! ${FILE} created."
+fi
+
 
 if ! [ -d "$DATA_DIR" ]; then
     echo "Setting up $DATA_DIR directory..."
@@ -10,7 +33,7 @@ if ! [ -d "$DATA_DIR" ]; then
     echo "Completed! $DATA_DIR directory created."
 fi
 
-COMMANDS='#!/bin/bash\nTARGET_DIR\=\$(pwd)\nTARGET_DIR=\${TARGET_DIR}/gmc/gmc_code\n'
+COMMANDS='#!/bin/bash\nTARGET_DIR\=\$(pwd)\nTARGET_DIR=\${TARGET_DIR}/rmgm_code/gmc/gmc_code\n'
 declare -a GMC_TASKS=("rl" "supervised" "unsupervised")
 declare -a RMGM_DATASETS=("pendulum"  "mosi_mosei" "mhd")
 declare -a FILE_LIST=("train_pendulum_dataset_samples20000_stack2_freq440.0_vel20.0_rec['LEFT_BOTTOM', 'RIGHT_BOTTOM', 'MIDDLE_TOP'].pt;test_pendulum_dataset_samples2000_stack2_freq440.0_vel20.0_rec['LEFT_BOTTOM', 'RIGHT_BOTTOM', 'MIDDLE_TOP'].pt;" "mosei_test_a.dt;mosi_test_a.dt;mosei_valid_a.dt;mosei_train_a.dt;mosi_train_a.dt;mosi_valid_a.dt;" "mhd_test.pt;mhd_train.pt;")
@@ -23,18 +46,18 @@ for idx in "${!GMC_TASKS[@]}"; do
     file_diff=$(diff <(echo "$idx_file_list") <(echo "$gmc_dataset_files"))
 
     if [ "$file_diff" != "" ]; then
-        download_script=${CUR_DIR}/gmc/gmc_code/download_${task}_dataset.sh
+        download_script=${CUR_DIR}/rmgm_code/gmc/gmc_code/download_${task}_dataset.sh
 
         if IFS= read -r firstline < "$download_script" && ! [[ $firstline == '#!/bin/bash' ]]; then
             echo "Making ${download_script} executable..."
-            sed -i "/${firstline}/i ${COMMANDS}" $downpendulumload_script
+            sed -i "/${firstline}/i ${COMMANDS}" $download_script
             sed -i -e 's/-O ./-O ${TARGET_DIR}/g' $download_script
             chmod u+x $download_script
             echo "Completed! The script ${download_script} is now executable."
         fi
 
         echo "Downloading data to ${GMC_DIR}/${task}/dataset..."
-        bash ${download_script}
+        bash $download_script
         echo "Completed! Downloaded the following files: ${idx_file_list}."
     fi
 
@@ -56,5 +79,7 @@ for idx in "${!GMC_TASKS[@]}"; do
 done
 
 sed -i -e "s|^m_path.*$|m_path = \"${CUR_DIR}/rmgm_code\"|g" ${CUR_DIR}/rmgm_code/main.py
+
+rm -rf ${CUR_DIR}/rmgm_code/gmc
 
 echo "Setup completed!"
