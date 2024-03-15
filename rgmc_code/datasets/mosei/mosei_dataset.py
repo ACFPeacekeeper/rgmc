646ef0e3ec54060703a6b2cbe43b3f94cba14889
MrIceHavoc <afonso.fernandes100@gmail.com>
2024-03-15 21:02:25 +0000

    Saves MOSI and MOSEI datasets to torch tensor

--- rgmc_code/datasets/mosei/mosei_dataset.py
+++ rgmc_code/datasets/mosei/mosei_dataset.py
@@ -11,22 +11,26 @@ class MoseiDataset(MultimodalDataset):
     @staticmethod
     def _download():
         run([os.path.join(os.getcwd(), "datasets", "mosei", "download_mosei_dataset.sh"), "bash"], shell=True)
+        dataset_dir = os.path.join(os.getcwd(), "datasets", "mosei")
+        data = torch.load(os.path.join(dataset_dir, "mosei_train.dt"))
+        val_data = torch.load(os.path.join(dataset_dir, "mosei_valid.dt"))
+        dataset = {'text': torch.concat((data.text, val_data.text)), 'audio': torch.concat((data.audio, val_data.audio)), 'vision': torch.concat((data.vision, val_data.vision)), 'labels': torch.concat((data.labels, val_data.labels))}
+        torch.save(dataset, os.path.join(dataset_dir, "mosei_train.pt"))
+        data = torch.load(os.path.join(dataset_dir, "mosei_test.dt"))
+        dataset = {'text': data.text, 'audio': data.audio, 'vision': data.vision, 'labels': data.labels}
+        torch.save(dataset, os.path.join(dataset_dir, "mosei_test.pt"))
         return
     
     def _load_data(self, train):   
         if train:
-            data_path = os.path.join(self.dataset_dir, "mosei_train.dt")
-            data = torch.load(data_path)
-            val_data_path = os.path.join(self.dataset_dir, "mosei_valid.dt")
-            val_data = torch.load(val_data_path)
-            self.dataset = {'text': torch.concat((data.text, val_data.text)).to(self.device), 'audio': torch.concat((data.audio, val_data.audio)).to(self.device), 'vision': torch.concat((data.vision, val_data.vision)).to(self.device)}
-            self.labels = torch.concat((data.labels, val_data.labels)).to(self.device)
+            data_path = os.path.join(self.dataset_dir, "mosei_train.pt")
             
         else:
-            data_path = os.path.join(self.dataset_dir, "mosei_test.dt")
-            data = torch.load(data_path)
-            self.dataset = {'text': data.text.to(self.device), 'audio': data.audio.to(self.device), 'vision': data.vision.to(self.device)}
-            self.labels = data.labels.to(self.device)
+            data_path = os.path.join(self.dataset_dir, "mosei_test.pt")
+
+        data = torch.load(data_path)
+        self.dataset = {'text': data['text'].to(self.device), 'audio': data['audio'].to(self.device), 'vision': data['vision'].to(self.device)}
+        self.labels = data['labels'].to(self.device)
 
         self.dataset_len = len(self.labels)
 
