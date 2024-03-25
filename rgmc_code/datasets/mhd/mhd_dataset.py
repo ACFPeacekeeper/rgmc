8a2a9aca505e5fdc20d7d4bcba41c74cd3770dea
MrIceHavoc <afonso.fernandes100@gmail.com>
2024-03-25 22:56:49 +0000

    Updated mhd dataset with size description

--- rgmc_code/datasets/mhd/mhd_dataset.py
+++ rgmc_code/datasets/mhd/mhd_dataset.py
@@ -12,7 +12,8 @@ class MhdDataset(MultimodalDataset):
 
     @staticmethod
     def _download():
-        run([os.path.join(os.getcwd(), "datasets", "mhd", "download_mhd_dataset.sh"), "bash"], shell=True)
+        dataset_dir = os.path.join(os.getcwd(), "datasets", "mhd")
+        run([os.path.join(dataset_dir, "download_mhd_dataset.sh"), "bash"], shell=True)
         return
 
     def _load_data(self, train):
@@ -27,15 +28,19 @@ class MhdDataset(MultimodalDataset):
         # Normalize datasets
         data[1] = (data[1] - torch.min(data[1])) / (torch.max(data[1]) - torch.min(data[1]))
         data[2] = (data[2] - torch.min(data[2])) / (torch.max(data[2]) - torch.min(data[2]))
+        data[3] = (data[3] - torch.min(data[3])) / (torch.max(data[3]) - torch.min(data[3]))
+        self.dataset = {
+            'image': data[1].to(self.device),           # size (n_samples x n_channels x npixels_width x npixels_height) = 50000||10000 x 1 x 28 x 28
+            'trajectory': data[2].to(self.device),      # size (n_samples x xy_coords [x_1, y_1, x_2, y_2, ..., x_100, y_100]) = 50000||10000 x 200 
+            'sound': data[3].to(self.device),           # size (n_samples x n_channels x ? x ?) = 50000||10000 x 4 x 32 x 32
+            'max': [data[4]['max'], data[5]['max']],    # [float, np.float32]
+            'min': [data[4]['min'], data[5]['min']]     # [float, np.float32]
+        }
+        self.labels = data[0].to(self.device)           # size (n_samples) = 50000||10000 (int value)
+        
+        if self.exclude_modality != 'none' and self.exclude_modality is not None:
+            self.dataset[self.exclude_modality] = torch.full(self.dataset[self.exclude_modality], -1).to(self.device)
 
-        if self.exclude_modality == 'image':
-            self.dataset = {'image': torch.full(data[1].size(), -1).to(self.device),'trajectory': data[2].to(self.device)}
-        elif self.exclude_modality == 'trajectory':
-            self.dataset = {'image': data[1].to(self.device), 'trajectory': torch.full(data[2].size(), -1).to(self.device)}
-        else:
-            self.dataset = {'image': data[1].to(self.device), 'trajectory': data[2].to(self.device)}
-
-        self.labels = data[0].to(self.device)
         return
     
     def _show_dataset_label_distribution(self):
